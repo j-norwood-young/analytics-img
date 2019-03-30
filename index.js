1678157263d54b599ca9f1653cd6762dcf59b228
Jason Norwood-Young <jason@10layer.com>
2019-03-30 16:01:09 +0100

    Initial checkin - early tests show promise for this subject

---
+++ index.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const http = require('http')
+const fs = require('fs')
+const Cookies = require('cookies')
+const uuid = require('uuid/v1')
+const axios = require("axios")
+
+if (!process.env.GA) {
+    console.error("GA required");
+    process.exit(1);
+}
+
+const filename = process.env.FILENAME || "1px-transparent.png"
+const port = process.env.PORT || 4011
+const host = process.env.HOST || "127.0.0.1"
+const secret = process.env.SECRET || "verysecret"
+const gaurl = process.env.GAURL || "https://www.google-analytics.com/collect"
+
+var hits = 0
+
+class AnalyticsCollect {
+    constructor(ga) {
+        if (!ga) throw("ga required");
+        this.ga = ga;
+    }
+
+    async hit(req, res) {
+        const cookies = new Cookies(req, res, { keys: [ secret ] })
+        var uid = cookies.get('uid', { signed: true })
+        if (!uid) {
+            uid = uuid();
+            cookies.set('uid', uid, { signed: true })
+        }
+        console.log(req.headers)
+        const data = {
+            v: 1,
+            tid: this.ga,
+            uid,
+            t: "pageview",            
+            ua: req.headers["user-agent"],
+            uip: req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null),
+            dr: req.headers.referer,
+            dp: "/stories/property-developer-rides-the-jobs-fund-train",
+            dh: "property-developer-rides-the-jobs-fund-train",
+        }
+        console.log(data);
+        try {
+            const result = await axios.post(gaurl, data)
+            console.log(result.status, result.statusText);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+}
+
+const analytics = new AnalyticsCollect(process.env.GA);
+
+http.createServer((req, res) => {
+    if (req.url == '/favicon.ico') return;
+    analytics.hit(req, res)
+    res.writeHead(200, { "Content-Type" : "image/png" })
+    const fileStream = fs.createReadStream(filename);
+    fileStream.pipe(res);
+    hits++;
+}).listen(port, host, () => {
+    console.log(`Server listening ${ host }:${ port }`);
+});
+
+setInterval(() => { console.log(`Hits: ${ hits }`)}, 10000)
